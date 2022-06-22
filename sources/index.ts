205d4151b0141d6b619235fb08931c7754377a0e
Samuel Lopez <samuel.lopez@90poe.io>
2022-06-22 12:19:05 +0100

    feat: return early if mode update-lockfile

--- sources/index.ts
+++ sources/index.ts
@@ -1,29 +1,42 @@
-import { Plugin, Project, scriptUtils } from '@yarnpkg/core'
+import { Plugin, Project, scriptUtils } from "@yarnpkg/core";
+import { InstallOptions } from "@yarnpkg/core/lib/Project";
 
-const scriptName = 'postinstallDev'
+const scriptName = "postinstallDev";
 
 // @see https://github.com/yarnpkg/berry/blob/d7d260e4ee6e39ed4b3dac1fdabbed025bf99899/packages/plugin-essentials/sources/commands/run.ts#L82
 const plugin: Plugin = {
   hooks: {
-    async afterAllInstalled(project: Project) {
-      const locator = project.topLevelWorkspace.anchoredLocator
-      if (await scriptUtils.hasPackageScript(locator, scriptName, { project })) {
-        const exitCode = await scriptUtils.executePackageScript(locator, scriptName, [], {
-          project,
-          stdin: process.stdin,
-          stdout: process.stdout,
-          stderr: process.stderr
-        })
+    async afterAllInstalled(project: Project, options: InstallOptions) {
+      if (options.mode === "update-lockfile") {
+        return;
+      }
+      const locator = project.topLevelWorkspace.anchoredLocator;
+      if (
+        await scriptUtils.hasPackageScript(locator, scriptName, { project })
+      ) {
+        const exitCode = await scriptUtils.executePackageScript(
+          locator,
+          scriptName,
+          [],
+          {
+            project,
+            stdin: process.stdin,
+            stdout: process.stdout,
+            stderr: process.stderr,
+          }
+        );
 
         if (exitCode !== 0) {
-          const error = new Error(`${scriptName} script failed with exit code ${exitCode}`)
-          error.stack = undefined
+          const error = new Error(
+            `${scriptName} script failed with exit code ${exitCode}`
+          );
+          error.stack = undefined;
 
-          throw error
+          throw error;
         }
       }
     },
   },
-}
+};
 
-export default plugin
+export default plugin;
